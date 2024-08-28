07489bf21b65bae44cb1febe8b498382cd6c82be
canifty <can-dindar@outlook.com>
2024-08-28 07:23:46 -0400

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Can Dindar on 2024-08-28.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
